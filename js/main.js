010150e0c5b8302e7c9b97519aac69f3c897e955
princeTD <talukderprince100@gmail.com>
2021-09-02 21:14:11 +0600

    added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const searchBtn = () => {
-    const error = document.getElementById('error')
+    const error = document.getElementById('error');
+  
     const searchFild = document.getElementById('search-input');
     const searchText = searchFild.value;
     // clear data
@@ -21,7 +22,7 @@ const searchBtn = () => {
 };
 
 const searchResult = book => {
-    
+    // console.log(book)
     const error = document.getElementById('error');
     const searchResult = document.getElementById("display-result");
     searchResult.textContent="";
@@ -34,33 +35,28 @@ const searchResult = book => {
     else{
         error.innerHTML= "";
     }
+    const foundTotal = document.getElementById("found-total");
+    foundTotal.innerText= `${book.length}`
 
     book.forEach(book =>{
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML=`
         <div class="col">
-        <div class="card h-100">
-          <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="..." height="400px" width="100px">
-          <div class="card-body">
-            <h5 class="card-title">${book.title.slice(0, 25)}</h5>
-            <p>Author Name: ${book.author_name} <br>  <span>First Publish Year: ${book.first_publish_year}</span>
-           
-            </p>
-           
-          </div>
+            <div class="card h-100">
+                 <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="..." height="400px" width="100px">
+                <div class="card-body">
+                    <h5 class="card-title">${book.title.slice(0, 25)}</h5>
+                    <p>Author Name: ${book.author_name} <br>  <span>First Publish Year: ${book.first_publish_year}</span>
+                
+                    </p>
+                
+                </div>
+            </div>
         </div>
-      </div>
         `
-        searchResult.appendChild(div)
-    
-        
-        // total found book detils show
-        // const foundTotal = document.getElementById('found-total');
-        // const span = document.createElement('span');
-        // span.innerHTML=`
-        // <span>${book.length}</span>
-        // ` 
-        // foundTotal.appendChild(span);
+        searchResult.appendChild(div);
+
+      
     })
 };
